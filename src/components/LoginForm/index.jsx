8be447c5442a1d4bb02aac2a947ef7b18624c6d4
DevBraz <engcivilbraz@hotmail.com>
2022-06-21 21:53:29 -0300

    feat:login
    Container de login feito.

---
+++ src/components/LoginForm/index.jsx
@@ -0,0 +1,51 @@
+import { FormContainer } from "../LoginForm/styles"
+import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup';
+import InputForm from '../../components/InputForm'
+import ButtonForm from '../../components/InputForm'
+// import { toast } from "react-toastify";
+
+
+const LoginForm = ({}) => {
+
+  // const onSubmitFunction = (data) => {
+  //   .then((res) => {
+  //   }).catch((err) => toast.error('Email ou senha inválido') )
+  // }
+
+  const schema = yup.object().shape({
+    email: yup
+      .string()
+      .email('Email inválido')
+      .required('Campo obrigatório!'),
+    password: yup
+      .string()
+      .min(8, 'Minimo 8 digitos')
+      .required('Campo obrigatório!'),
+  })
+
+  const { 
+    register, 
+    handleSubmit, 
+    formState:{errors} 
+  } = useForm({
+    resolver: yupResolver(schema)
+  })
+
+  return (
+
+    <FormContainer onSubmit={handleSubmit(onSubmitFunction)}>
+      <h3>Login</h3>
+      <InputForm type="text" {...register('email')}/>
+      {errors.email?.message}
+      <InputForm type="password" {...register('password')} />
+      {errors.password?.message}
+      <ButtonForm type="submit">Entrar</ButtonForm>
+    </FormContainer>
+
+  )
+
+}
+
+export default LoginForm
---
+++ src/components/LoginForm/styles.js
@@ -0,0 +1,14 @@
+
+
+
+export const FormContainer = styled.form`
+  
+  display: flex;
+  flex-direction: column;
+  background-color: var(--color-purple);
+
+  h3 {
+    font-size: clamp(1.5rem, 8vw, 2.5rem);
+  }
+
+`
