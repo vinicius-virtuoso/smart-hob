1a027ad0bbf670f26e4794277bc8c8188bae10f8
Vinicius Virtuoso <viniciusvirtuoso1902@gmail.com>
2022-06-21 23:14:26 -0300

    fix(): juste e importação do css do toastify

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { ToastContainer } from "react-toastify";
 import App from "./App";
 import { GlobalStyle } from "./Style/global";
+import "react-toastify/dist/ReactToastify.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
