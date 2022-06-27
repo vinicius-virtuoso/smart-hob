dbb0391724354a736137b0c31d362a4d5384a65d
DevBraz <engcivilbraz@hotmail.com>
2022-06-27 16:23:30 -0300

    card groups
    Feito card groups básico

---
+++ src/components/CardGroups/index.jsx
@@ -0,0 +1,23 @@
+import { ButtonForm } from "../ButtonForm";
+import { Container, TextDiv, ButtomDiv } from "./styles";
+
+const CardGroups = () => {
+
+  return (
+
+    <Container>
+      <TextDiv>
+        <h4>Group Name</h4>
+        <h5>Group Type</h5>
+        <h6>Group Description</h6>
+      </TextDiv>
+      <ButtomDiv>
+        <ButtonForm primary>Seguir</ButtonForm>
+      </ButtomDiv>
+    </Container>
+
+  );
+
+};
+
+export default CardGroups;
---
+++ src/components/CardGroups/styles.js
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 600px;
+  height: 75px;
+  background-color: var(--color-card-groups-background);
+  border-radius: 5px;
+  padding: 10px 20px 10px 40px;
+
+
+
+`
+
+export const TextDiv = styled.div`
+
+  display: flex;
+  height: 100%;
+  width: 75%;
+  flex-direction: column;
+  justify-content: space-around;
+
+  h4 {
+    font-family: 'Barlow';
+    font-size: clamp(0.5rem, 20vw, 0.9rem);
+  }
+
+  h5 {
+    font-family: 'Barlow';
+    font-size: clamp(0.5rem, 20vw, 0.7rem);
+  }
+
+  h6 {
+    font-family: 'Barlow';
+    font-size: clamp(0.5rem, 20vw, 0.5rem);
+  }
+
+
+`
+
+export const ButtomDiv = styled.div`
+
+  height: 100%;
+  width: 25%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: center;
+
+  button {
+    font-family: 'Poppins';
+    font-size: clamp(0.5rem, 20vw, 0.7rem);
+    width: 80px;
+    height: 35px;
+    min-height: 0;
+    font-weight: bold;
+  }
+
+
+`
+
