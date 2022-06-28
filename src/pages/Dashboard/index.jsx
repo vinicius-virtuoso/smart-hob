068a0dfc1f980062158c3f94a52b5433782d0b4e
DevBraz <engcivilbraz@hotmail.com>
2022-06-28 20:46:57 -0300

    atualização
    atualizado erros segundo QA

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import CardHobbies from "../../components/CardHobbies";
 
 function Dashboard() {
   const navigate = useNavigate();
   return (
     <div>
       <h1>Dashboard</h1>
+      <CardHobbies></CardHobbies>
       <button
         onClick={() => {
           localStorage.clear();
