b67abef95f0f95e55f359523b75932bf43132af3
Vinicius Virtuoso <viniciusvirtuoso1902@gmail.com>
2022-06-29 18:43:26 -0300

    refactor(): modal resposivo

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { ModalUser } from "../../components/ModalUser";
 
 function Dashboard() {
   const navigate = useNavigate();
   return (
     <div>
       <h1>Dashboard</h1>
+      <ModalUser />
       <button
         onClick={() => {
           localStorage.clear();
