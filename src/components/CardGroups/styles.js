84952acfceb2f49ba738f0a0c68087a5b869c083
Vinicius Virtuoso <viniciusvirtuoso1902@gmail.com>
2022-06-30 16:50:56 -0300

    remove(): importações não usadas removidas

--- src/components/CardGroups/styles.js
+++ src/components/CardGroups/styles.js
@@ -8,16 +8,9 @@ export const Container = styled.div`
   height: 100%;
   background-color: var(--color-card-groups-background);
   border-radius: 5px;
-<<<<<<< HEAD
   padding: 1rem 2rem;
   align-items: center;
 `;
-=======
-  padding: 10px 20px 10px 40px;
- 
-
-`
->>>>>>> 72b8dda39d467039d5e4c1372a8f7bc0342d9681
 
 export const TextDiv = styled.div`
   display: flex;
@@ -31,7 +24,6 @@ export const TextDiv = styled.div`
   gap: 0.3rem;
 
   h3 {
-<<<<<<< HEAD
     font-size: 1.5rem;
   }
 
@@ -42,20 +34,6 @@ export const TextDiv = styled.div`
   p {
     font-size: 0.875rem;
     opacity: 0.8;
-=======
-    font-family: 'Barlow';
-    font-size: clamp(0.5rem, 20vw, 0.9rem);
-  }
-
-  span {
-    font-family: 'Barlow';
-    font-size: clamp(0.5rem, 20vw, 0.7rem);
-  }
-
-  p {
-    font-family: 'Barlow';
-    font-size: clamp(0.5rem, 20vw, 0.5rem);
->>>>>>> 72b8dda39d467039d5e4c1372a8f7bc0342d9681
   }
 `;
 
@@ -74,11 +52,5 @@ export const ButtonDiv = styled.div`
     height: 35px;
     min-height: 0;
     font-weight: bold;
-    transition: none;
-    :hover {
-      
-      background-color: var(--color-dark);
-      color: var(--color-light);
-    }
   }
 `;
