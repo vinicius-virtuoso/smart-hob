948d087317348da7f65290a7c70b3edcd9811f24
Vinicius Virtuoso <viniciusvirtuoso1902@gmail.com>
2022-07-06 01:59:09 -0300

    refactor(): ajuste do modal de frases agr com animação

--- src/components/ModalPhrase/index.jsx
+++ src/components/ModalPhrase/index.jsx
@@ -1,29 +1,27 @@
 import { useContext } from "react";
 import { AiFillCloseCircle } from "react-icons/ai";
-import { Container, Div } from "./styled";
+import { Container, Div, Box } from "./styled";
 import { ShowPhraseContext } from "../../Context/Provider/ShowModal";
 
 const ModalPhrase = () => {
   const { openPhrase, close_phrase, phrase } = useContext(ShowPhraseContext);
 
   return (
-    <>
-      {!!openPhrase ? (
-        <Container>
-          <Div>
-            <div>
-              <h5>Frase do Dia</h5>
-              <p>{phrase || "Viva o hoje como se fosse o ultimo."}</p>
-            </div>
-            <button onClick={close_phrase}>
-              <p>
-                <AiFillCloseCircle />
-              </p>
-            </button>
-          </Div>
-        </Container>
-      ) : null}
-    </>
+    <Box openPhrase={openPhrase}>
+      <Container>
+        <Div>
+          <div>
+            <h5>Frase do Dia</h5>
+            <p>{phrase || "Viva o hoje como se fosse o ultimo."}</p>
+          </div>
+          <button onClick={close_phrase}>
+            <p>
+              <AiFillCloseCircle />
+            </p>
+          </button>
+        </Div>
+      </Container>
+    </Box>
   );
 };
 export default ModalPhrase;
--- src/components/ModalPhrase/styled.js
+++ src/components/ModalPhrase/styled.js
@@ -1,46 +1,56 @@
 import styled from "styled-components";
 
+export const Box = styled.div`
+  width: 100%;
+  overflow: hidden;
+  max-height: 200px;
+  background-color: var(--color-pink-light);
+  margin-top: 0.25rem;
+
+  transition: all 0.3s ease-in-out;
+
+  ${(props) => (props.openPhrase === true ? "height:100%" : "height:0%;")}
+`;
+
 export const Container = styled.div`
-width: 100%;
-background-color: var(--color-pink-light);
-display: flex;
-align-items: center;
-justify-content: center;
-margin-top: 10px;
-padding-bottom: 10px;
+  width: 100%;
 
-`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  padding-bottom: 10px;
+`;
 export const Div = styled.div`
-min-height: 150px;
-width: 95%;
-display: flex;
-flex-direction: column-reverse;
-justify-content: center;
-align-items: center;
+  min-height: 150px;
+  width: 95%;
+  display: flex;
+  flex-direction: column-reverse;
+  justify-content: center;
+  align-items: center;
 
-div{
+  div {
     width: 100%;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
     font-family: var(--fml-Poppins);
-    h5{
-        font-size: clamp(1.3rem, 15vw, 1.6rem);
-        margin-bottom: 15px;
-        color: var(--color-purple);
-
+    h5 {
+      font-size: clamp(1.3rem, 15vw, 1.6rem);
+      margin-bottom: 15px;
+      color: var(--color-purple);
     }
-    p{
-        color: var(--color-dark);
-        font-weight: bold;
-        letter-spacing: 0.05em;
-        font-size: clamp(1rem, 15vw, 1.2rem);
-        text-align: center;
+    p {
+      color: var(--color-dark);
+      font-weight: bold;
+      letter-spacing: 0.05em;
+      font-size: clamp(1rem, 15vw, 1.2rem);
+      text-align: center;
     }
-}
+  }
 
-button{
+  button {
     margin-top: 10px;
     width: 100%;
     background: none;
@@ -51,13 +61,11 @@ button{
     display: flex;
     align-items: center;
     justify-content: end;
-    p{
-        display: flex;
-    align-items: center;
-    justify-content: end;
-        color: var(--color-gray);        
-
+    p {
+      display: flex;
+      align-items: center;
+      justify-content: end;
+      color: var(--color-gray);
     }
-}
-
-`
+  }
+`;
